e9fd4f8aeeb949b848787978aa5dee4f1bc3ee4b
Ammar Khwaira <ammar.khwaira@gmail.com>
2022-03-17 16:33:36 +0300

    finalizing main.py

--- main.py
+++ main.py
@@ -5,19 +5,12 @@ if __name__ == '__main__':
     except:
         logger.error(f"Extract: uncaught exception in main: {traceback.format_exc()}")
 
-<<<<<<< HEAD
-
 print('master message1')
-
-
 
 
 def func2:
 	print('master added func2')
-=======
-print('main_fix1 message1')
 
 
 def func1:
 	print('main_fix1 added func1')
->>>>>>> main_fix1
