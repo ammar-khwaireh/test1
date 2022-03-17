4b61975e396989eb73ff9ee9c7932a6be895d8fa
Ammar Khwaira <ammar.khwaira@gmail.com>
2022-03-17 11:21:56 +0300

    add main_fix1 to log message

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 if __name__ == '__main__':
     try:
-        logger.info('ETL Process Initialized')
+        logger.info('ETL Process Initialized for main_fix1')
         main()
     except:
         logger.error(f"Extract: uncaught exception in main: {traceback.format_exc()}")
