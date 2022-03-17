9a892511a3b9f555ae5aeb1bc2f578ad14f828e0
Ammar Khwaira <ammar.khwaira@gmail.com>
2022-03-17 11:32:38 +0300

    add inline comment next to main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 if __name__ == '__main__':
     try:
         logger.info('ETL Process Initialized for main_fix1')
-        main()
+        main() #inline comment for main_fix1
     except:
         logger.error(f"Extract: uncaught exception in main: {traceback.format_exc()}")
